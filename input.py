91b73e507bf96f223efeddd8a5291c4703e42b51
Ankit <ankitmaurya20223@gmail.com>
2025-02-16 12:49:51 +0530

    Added input.py

---
+++ input.py
@@ -0,0 +1,24 @@
+a = input("Enter number 1: ") # this will input the number as a string not as a number
+# if input of a is: 1
+
+b = input("Enter number 2: ") # this will input the number as a string not as a number
+# if input of b is: 2
+
+print("Number a is: ", a)
+
+print("Number b is: ", b)
+
+print("Sum is: ", a + b) # this will concatenate the two strings not add them
+# Output of this code will be: 12
+
+
+# To convert the input to number we can use int() or float() function
+a = int(input("Enter number 1: ")) # this will input the number as a number not as a string
+
+b = int(input("Enter number 2: ")) # this will input the number as a number not as a string
+
+print("Number a is: ", a)
+
+print("Number b is: ", b)
+
+print("Sum is: ", a + b) # this will add the two numbers and doesn't concatenate it
---
+++ introduction_to_python.py
@@ -0,0 +1,15 @@
+a = 1
+
+b = 2.0
+
+c = "Ankit"
+
+print(a, b, c)
+
+print(type(a), type(b), type(c))
+
+print(a + b)
+
+d = float(a)
+
+print(d, type(d))
